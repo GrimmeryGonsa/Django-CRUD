338add6acbbf127a16222c1d2422effc9b984254
GrimmeryGonsa <saumya2234@gmail.com>
2020-10-21 17:06:17 +0530

    Initial commit...front/index page yet to be made

---
+++ crud/employee_register/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from employee_register.models import *
+
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Position)
+
---
+++ crud/employee_register/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.employee_form,name='employee_insert'),#get and post request for insert operation
+    path('list/',views.employee_list,name='employee_list'),#get and post request for update operation
+    path('<int:id>/',views.employee_form,name='employee_update'),#get request to retrive and dislay all records
+    path('delete/<int:id>/',views.employee_delete,name="employee_delete" )
+
+    
+]
